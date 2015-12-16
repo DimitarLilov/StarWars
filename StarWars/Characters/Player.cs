ddd7dadf6cfe4b01921c29bafc01097714d44081
AlexTinev <alexandertinev@gmail.com>
2015-12-16 14:15:50 +0200

    AddContracts

--- StarWars/Characters/Player.cs
+++ StarWars/Characters/Player.cs
@@ -1,13 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using StarWars.Contracts;
 namespace StarWars.Characters
 {
-    class Player 
+    class Player : IPlayer
     {
+        public int Health
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
 
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable<ICollectible> Inventory
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Position position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public PlayerRace Race
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Attack(ICharacter enemy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CollectItem(ICollectible item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetHealth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(ConsoleKeyInfo pressedKey)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ StarWars/Contracts/ICollect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars.Contracts
+{
+    interface ICollect
+    {
+        void CollectItem(ICollectible item);
+
+        IEnumerable<ICollectible> Inventory { get; }  
+    }
+}
