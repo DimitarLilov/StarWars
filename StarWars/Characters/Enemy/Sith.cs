f3711d62008a9668c756faef17178b9ef460a23c
AlexTinev <alexandertinev@gmail.com>
2015-12-17 17:02:50 +0200

    Mapp class extended

---
+++ StarWars/Characters/Enemy/Sith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StarWars.GameObject;
+
+namespace StarWars.Characters.Enemy
+{
+    public class Sith : Character
+    {
+        private const int SithDamage = 400;
+        private const int SithHealth = 400;
+        public Sith(Position position, char symbol, string name) : base(position, symbol, SithDamage, SithHealth, name)
+        {
+        }
+    }
+}
---
+++ StarWars/Characters/Enemy/StormTrooper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StarWars.GameObject;
+
+namespace StarWars.Characters.Enemy
+{
+    public class StormTrooper : Character
+    {
+        private const int StormTrooperDamage = 100;
+        private const int StormTrooperHealth = 100;
+        public StormTrooper(Position position, char symbol, string name) : base(position, symbol, StormTrooperDamage, StormTrooperHealth, name)
+        {
+        }
+    }
+}
